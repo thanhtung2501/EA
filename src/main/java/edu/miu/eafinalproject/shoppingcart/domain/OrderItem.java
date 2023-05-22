315fdbdc24c6c9a4368274acce99d35b6ab89b6d
Tung Le <thanhtung2501@gmail.com>
2023-05-22 09:37:51 -0500

    calculate total price and refactor

--- src/main/java/edu/miu/eafinalproject/shoppingcart/domain/OrderItem.java
+++ src/main/java/edu/miu/eafinalproject/shoppingcart/domain/OrderItem.java
@@ -5,25 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderItem {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "product_id")
     private Product product;
-
     private int quantity;
     private double discountValue;
-
-    @ManyToOne
-    @JoinColumn(name = "order_id")
     private Orders orders;
 }

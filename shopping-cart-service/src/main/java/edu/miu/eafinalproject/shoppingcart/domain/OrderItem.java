df4f2471c8b3881a45c10bf97eb42508b8b9e12d
Tung Le <thanhtung2501@gmail.com>
2023-05-23 17:25:04 -0500

    added apigateway

--- shopping-cart-service/src/main/java/edu/miu/eafinalproject/shoppingcart/domain/OrderItem.java
+++ shopping-cart-service/src/main/java/edu/miu/eafinalproject/shoppingcart/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package edu.miu.eafinalproject.shoppingcart.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 //@Entity
 public class OrderItem {
 //    @Id

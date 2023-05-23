d407a5f430bb1990ce3bba27bb779ebc6b00d170
kalab Afera <kalabafex@gmail.com>
2023-05-23 11:51:56 -0500

    service

--- customer-service/src/main/java/edu/miu/eafinalproject/services/ReviewService.java
+++ customer-service/src/main/java/edu/miu/eafinalproject/services/ReviewService.java
@@ -2,9 +2,10 @@ package edu.miu.eafinalproject.services;
 
 import edu.miu.eafinalproject.data.ReviewDTO;
 import edu.miu.eafinalproject.domain.Review;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface ReviewService {
     ReviewDTO getReview(Long reviewId);
     ReviewDTO updateReview(Review review);
